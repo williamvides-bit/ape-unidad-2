b26f69fe88796f9b33bc5ac77aaaaade2c6ffc94
william vides <wilx.sv@gmail.com>
2026-03-10 21:59:11 -0600

    Etiquetas, Botones y Cuadros de Texto
    Son los componentes más básicos de interacción

---
+++ EtiquetasBotonesCuadrosTexto/App/src/main/java/sv/edu/ues/ape115/app/App.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sv.edu.ues.ape115.app;
+import javax.swing.*;
+
+/**
+ * Etiquetas, Botones y Cuadros de Texto
+ */
+public class App {
+
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame("Ejemplo Swing");
+
+        JLabel etiqueta = new JLabel("Nombre:");
+        JTextField campo = new JTextField(15);
+        JButton boton = new JButton("Saludar");
+
+        JPanel panel = new JPanel();
+
+        panel.add(etiqueta);
+        panel.add(campo);
+        panel.add(boton);
+
+        ventana.add(panel);
+
+        ventana.setSize(300,150);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+    }
+}
